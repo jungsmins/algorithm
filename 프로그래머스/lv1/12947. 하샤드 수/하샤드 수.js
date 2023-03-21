4cb3c91fdb5c07fc9575cf6870b192ebc165c1c1
JungMin Park <103472777+jungsmins1@users.noreply.github.com>
2023-03-21 23:42:57 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,10 +1,11 @@
 function solution(x) {
     let y = x
     let sum = 0;
+    
     while(y > 0) {
         sum += y % 10
         y = Math.floor(y / 10)
     }
-    console.log(sum)
+    
     return x % sum === 0
 }
