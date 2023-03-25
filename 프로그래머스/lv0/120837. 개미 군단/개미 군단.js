97c007525e78d652f15e732007c14290d2e03fa4
JungMin Park <103472777+jungsmins1@users.noreply.github.com>
2023-03-25 23:55:55 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,17 +1,5 @@
 function solution(hp) {
-    let result = 0;
     
-    if (hp >= 5) {
-        result += (hp - hp % 5) / 5
-        hp = hp - (hp - hp % 5) 
-    }
-       
-    if (hp >= 3) {
-        result += (hp - hp % 3) / 3
-        hp = hp - (hp - hp % 3)
-    }
     
-    result += hp === 2 ? 2 : (hp === 1 ? 1 : 0)
-    
-    return result
+    return Math.floor(hp / 5) + Math.floor((hp % 5) / 3) + Math.floor(((hp % 5) % 3) / 1)
 }
