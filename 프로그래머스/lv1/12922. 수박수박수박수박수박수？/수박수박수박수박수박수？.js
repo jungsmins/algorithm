4ad2195c8388856721939162230ac6c350e554b4
JungMin Park <103472777+jungsmins1@users.noreply.github.com>
2023-03-29 23:46:20 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,11 +1,3 @@
 function solution(n) {
-    let str = '수'
-    for (let i = 1; i < n; i++) {
-        if (str[i - 1] === '수') {
-            str += '박'
-        } else if (str[i - 1] === '박') {
-            str += '수'
-        }
-    }
-    return str
+    return '수박'.repeat(n).slice(0, n)
 }
