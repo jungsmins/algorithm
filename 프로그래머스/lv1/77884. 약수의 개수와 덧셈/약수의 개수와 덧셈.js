10423f783f0cf95cfe828855272a2a0c9aacaf73
JungMin Park <103472777+jungsmins1@users.noreply.github.com>
2023-04-03 22:55:18 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.66 ms, Memory: 35.2 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -2,11 +2,11 @@ function solution(left, right) {
     let result = 0;
     
     const divisor = (num) => {
-        let arr = []
+        let count = 0;
         for (let i = 1; i <= num; i++) {
-            if (num % i === 0) arr.push(i) 
+            if (num % i === 0) count++
         }
-        return arr.length % 2 === 0 ? num : -num
+        return count % 2 === 0 ? num : -num
     }
     
     for (let i = left; i <= right; i++) {
