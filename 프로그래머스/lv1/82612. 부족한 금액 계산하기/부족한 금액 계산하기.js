c12875030e3bfcbc5640679279faf693eed0a1ff
JungMin Park <103472777+jungsmins1@users.noreply.github.com>
2023-04-05 22:54:51 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,9 @@
+function solution(price, money, count) {
+    let total = 0;
+    for (let i = 1; i <= count; i++) {
+        total += price * i
+    }
+    
+    if (money >= total) return 0
+    return total - money
+}
