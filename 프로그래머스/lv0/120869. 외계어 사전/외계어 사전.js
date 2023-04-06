4f6d411b26f4ef2861f1bafe3a79546b5dd4ca28
JungMin Park <103472777+jungsmins1@users.noreply.github.com>
2023-04-06 23:47:30 +0900

    [level 0] Title: 외계어 사전, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -2,11 +2,7 @@ function solution(spell, dic) {
     var answer = 0;
     // 1. dic 을 반복한다.
     // 2. dic에서 spell을 삭제한다.
-    let count = 0;
-    spell = spell.sort()
-    dic.forEach((str) => {
-        if(str.split('').sort().join('') === spell.join('')) count++
-    })
-    
-    return count > 0 ? 1 : 2
+    spell = spell.sort().join('')
+    dic = dic.filter(s => [...s].sort().join('') === spell)
+    return dic.length > 0 ? 1 : 2
 }
