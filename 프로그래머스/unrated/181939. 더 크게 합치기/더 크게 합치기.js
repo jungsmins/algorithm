e8caf11a250d3e94ee031551078653400927a560
JungMin Park <103472777+jungsmins1@users.noreply.github.com>
2023-05-04 22:45:10 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,5 +1,5 @@
 function solution(a, b) {
     const str1 = String(a) + String(b)
     const str2 = String(b) + String(a)
-    return str1 >= str2 ? Number(str1) : Number(str2)
+    return Math.max(str1, str2)
 }
