fe15be452cd2c35377b1ba43fe75657c4756587f
JungMin Park <103472777+jungsmins1@users.noreply.github.com>
2023-05-07 23:59:30 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,5 +1,7 @@
 function solution(my_string, is_suffix) {
-    const strLength = is_suffix.length
+//     const strLength = is_suffix.length
     
-    return my_string.slice(-is_suffix.length) === is_suffix ? 1 : 0
+//     return my_string.slice(-is_suffix.length) === is_suffix ? 1 : 0
+    
+    return my_string.endsWith(is_suffix) ? 1 : 0
 }
