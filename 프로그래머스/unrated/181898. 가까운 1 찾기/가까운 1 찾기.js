7f8a973cb64a5182175e0f79091d03c1c2a0463a
JungMin Park <103472777+jungsmins1@users.noreply.github.com>
2023-05-15 00:08:33 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,7 +1,3 @@
 function solution(arr, idx) {
-    arr = arr.slice(idx)
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === 1) return idx + i
-    }
-    return -1
+    return arr.indexOf(1, idx)
 }
