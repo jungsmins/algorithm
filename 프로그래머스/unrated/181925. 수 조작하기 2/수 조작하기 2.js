5777fb8fa79097e1b1754661e37dc34954b487f2
JungMin Park <103472777+jungsmins1@users.noreply.github.com>
2023-05-17 23:51:54 +0900

    [unrated] Title: 수 조작하기 2, Time: 8.01 ms, Memory: 41.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,10 @@
+function solution(numLog) {
+    let answer = '';
+    for (let i = 0; i < numLog.length; i++) {
+        if (numLog[i] === numLog[i - 1] + 1) answer += 'w'
+        if (numLog[i] === numLog[i - 1] - 1) answer += 's'
+        if (numLog[i] === numLog[i - 1] + 10) answer += 'd'
+        if (numLog[i] === numLog[i - 1] - 10) answer += 'a'
+    }
+    return answer
+}
