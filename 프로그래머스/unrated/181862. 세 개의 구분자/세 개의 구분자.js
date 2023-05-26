3035c0ef22f2cad01df336cc742089ea834ef0c5
JungMin Park <103472777+jungsmins1@users.noreply.github.com>
2023-05-26 23:43:29 +0900

    [unrated] Title: 세 개의 구분자, Time: 9.28 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,10 +1,3 @@
 function solution(myStr) {
-    const arr = ['a', 'b', 'c']
-    let str = ''
-    for (let i = 0; i < myStr.length; i++) {
-        if (!arr.includes(myStr[i])) str += myStr[i]
-        else if (arr.includes(myStr[i]) && !arr.includes(myStr[i - 1])) str += ' '
-    }
-    const answer = str.split(' ').filter((str) => str !== '')
-    return answer.length > 0 ? answer : ['EMPTY'] 
+    return myStr.match(/[^a-c]+/g)||['EMPTY']
 }
