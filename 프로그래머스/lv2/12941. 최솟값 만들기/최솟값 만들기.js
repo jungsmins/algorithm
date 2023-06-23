7f1170ccb4d57ae1e437f860e2dfc900bdd86d7e
JungMin Park <103472777+jungsmins1@users.noreply.github.com>
2023-06-23 23:25:01 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.73 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -2,8 +2,8 @@ function solution(A,B){
     var answer = 0;
 
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    A = A.sort((a, b) => a - b)
-    B = B.sort((a, b) => b - a)
+    A.sort((a, b) => a - b)
+    B.sort((a, b) => b - a)
     
     for (let i = 0; i < A.length; i++) {
         answer += A[i] * B[i]
