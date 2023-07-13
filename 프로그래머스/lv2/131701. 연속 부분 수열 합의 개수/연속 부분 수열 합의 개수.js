cf12645c71b3723ad9fcd12bf132d0fc7946f50c
JungMin Park <103472777+jungsmins@users.noreply.github.com>
2023-07-13 23:42:27 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1233.72 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -0,0 +1,20 @@
+function solution(elements) {
+    let arr = []
+    const Length = elements.length
+    const newElements = elements.concat(elements)
+
+    for (let i = 1; i < Length + 1; i++) {
+        for (let j  = 0; j < Length; j++) {
+            arr.push(sum(newElements.slice(j, i + j)))
+        }
+    }
+    
+    function sum(arr2) {
+        let num = 0;
+        arr2.forEach((n) => num += n)
+        return num
+    }
+        
+    
+    return [...new Set(arr)].length
+}
