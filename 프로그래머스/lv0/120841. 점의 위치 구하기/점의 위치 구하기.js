22642e7c381ceb367a2cf23d67efdc3ed465c9db
JungMin Park <103472777+jungsmins@users.noreply.github.com>
2023-08-29 23:58:15 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,6 +1,3 @@
 function solution(dot) {
-    if (dot[0] > 0 && dot[1] > 0) return 1;
-    else if (dot[0] > 0 && dot[1] < 0) return 4;
-    else if (dot[0] < 0 && dot[1] > 0) return 2;
-    else return 3
+    return dot[0] > 0 ? dot[1] > 0 ? 1 : 4 : dot[1] > 0 ? 2 : 3;
 }
